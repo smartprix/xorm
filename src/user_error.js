0f5cd088aa785df6263d29efe37e92d814f3e746
Rohit Gohri <rohit.gohri@smartprix.com>
2018-09-21 18:25:33 +0530

    rename customError to userError, use it for default export

--- src/user_error.js
+++ src/user_error.js
@@ -1,30 +1,21 @@
-class UserError extends Error {
-	constructor(message) {
-		super(message);
-		this.data = message;
-		this.name = 'UserError';
-	}
-}
-
-export default UserError;
-
 /**
  *
  * @param {string} modelName
  * @return {typeof Error}
  */
 function customUserError(modelName) {
-	class CustomError extends Error {
+	class UserError extends Error {
 		constructor(message) {
 			super(message);
 			this.data = message;
-			this.name = modelName ? `${modelName}Error` : 'UserError';
+			this.name = 'UserError';
 			this.model = modelName;
 		}
 	}
-	return CustomError;
+	return UserError;
 }
 
+export default customUserError('');
 export {
 	customUserError,
 };
